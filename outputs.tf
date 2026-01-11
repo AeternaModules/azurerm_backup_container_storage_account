cf633e6f23f351e6c3f76ac24d5d267c2c34789a
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:57:27 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "backup_container_storage_accounts" {
+  description = "All backup_container_storage_account resources"
+  value       = azurerm_backup_container_storage_account.backup_container_storage_accounts
+}
+
