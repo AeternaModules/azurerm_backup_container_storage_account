03718586c12eb0cb02ebdecae9a6da775b912317
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:00:47 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "backup_container_storage_accounts_id" {
+  description = "Map of id values across all backup_container_storage_accounts, keyed the same as var.backup_container_storage_accounts"
+  value       = { for k, v in azurerm_backup_container_storage_account.backup_container_storage_accounts : k => v.id }
+}
 output "backup_container_storage_accounts_recovery_vault_name" {
   description = "Map of recovery_vault_name values across all backup_container_storage_accounts, keyed the same as var.backup_container_storage_accounts"
   value       = { for k, v in azurerm_backup_container_storage_account.backup_container_storage_accounts : k => v.recovery_vault_name }
