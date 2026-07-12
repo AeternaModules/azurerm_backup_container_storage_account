f3cdbd559ef429b3a90001c0c2cd9182fbb01761
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:10 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "backup_container_storage_accounts" {
-  description = "All backup_container_storage_account resources"
-  value       = azurerm_backup_container_storage_account.backup_container_storage_accounts
-}
 output "backup_container_storage_accounts_recovery_vault_name" {
-  description = "List of recovery_vault_name values across all backup_container_storage_accounts"
-  value       = [for k, v in azurerm_backup_container_storage_account.backup_container_storage_accounts : v.recovery_vault_name]
+  description = "Map of recovery_vault_name values across all backup_container_storage_accounts, keyed the same as var.backup_container_storage_accounts"
+  value       = { for k, v in azurerm_backup_container_storage_account.backup_container_storage_accounts : k => v.recovery_vault_name }
 }
 output "backup_container_storage_accounts_resource_group_name" {
-  description = "List of resource_group_name values across all backup_container_storage_accounts"
-  value       = [for k, v in azurerm_backup_container_storage_account.backup_container_storage_accounts : v.resource_group_name]
+  description = "Map of resource_group_name values across all backup_container_storage_accounts, keyed the same as var.backup_container_storage_accounts"
+  value       = { for k, v in azurerm_backup_container_storage_account.backup_container_storage_accounts : k => v.resource_group_name }
 }
 output "backup_container_storage_accounts_storage_account_id" {
-  description = "List of storage_account_id values across all backup_container_storage_accounts"
-  value       = [for k, v in azurerm_backup_container_storage_account.backup_container_storage_accounts : v.storage_account_id]
+  description = "Map of storage_account_id values across all backup_container_storage_accounts, keyed the same as var.backup_container_storage_accounts"
+  value       = { for k, v in azurerm_backup_container_storage_account.backup_container_storage_accounts : k => v.storage_account_id }
 }
 
